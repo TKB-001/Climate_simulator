ce9805c8e8f0d3f12af551eb85cf77e2a15b0eca
TKB-001 <tyler.bartle@kc.school.nz>
2025-07-13 13:59:22 +1200

    made orbital_distance a device function

--- interplanetary.py
+++ interplanetary.py
@@ -1,9 +1,10 @@
 from config import *
-from numpy import sin, pi, cos, sqrt, arctan2
+from math import sin, pi, cos, sqrt, atan2
+from numba import cuda
 
 """citation for kepler's equation: N. M. Swerdlow, "Kepler's iterative solution to Kepler's equation," J. Hist. Astron., vol. 31, no. 4, pp. 339-341, 2000,
  doi: 10.1177/002182860003100404"""
-
+@cuda.jit(device=True)
 def orbital_distance(t, a, e, P_orb):
     """
     t      : time since periastron [seconds]
@@ -14,11 +15,11 @@ def orbital_distance(t, a, e, P_orb):
     """
     M = 2*pi * (t % P_orb) / P_orb
 
-    E = M.copy()
+    E = M
     for _ in range(8):
         E = E + (M - (E - e*sin(E))) / (1 - e*cos(E))
 
-    f = 2*arctan2(sqrt(1+e)*sin(E/2),
+    f = 2*atan2(sqrt(1+e)*sin(E/2),
                      sqrt(1-e)*cos(E/2))
 
     return a * (1 - e**2) / (1 + e*cos(f))
